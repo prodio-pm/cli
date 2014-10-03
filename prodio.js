8c2b60be9b52df1dc61e9ba4b1d18d4f59c80e72
Jeremy Darling <jeremy.darling@pearson.com>
2014-10-03 11:01:05 -0500

    Initial Commit

---
+++ prodio.js
@@ -0,0 +1,50 @@
+var
+  Liftoff = require('liftoff'),
+  Prodio = new Liftoff({
+    name: 'Prodio',
+    configName: '.prodio',
+    cwdFlag: 'cwd',
+    configPathFlag: 'config',
+    preloadFlag: 'require'
+  }),
+  path = require('path'),
+  settings = require('commander');
+
+var pjson = require(__dirname+'/package.json'),
+    cjson = (function(){
+      try{
+        if(env.configPath){
+          console.log('Loading config from: ', env.configPath);
+          process.chdir(env.configBase);
+          return require(env.configPath);
+        }else{
+          return {};
+        }
+      }catch(e){
+        return {};
+      }
+    })(),
+    defaults = pjson.defaults||{};
+
+settings
+  .version('v'+pjson.version, '-v, --version')
+  ;
+
+settings.unknownOption = (function(){
+  var cb = settings.unknownOption;
+  return function(opt){
+    switch(opt){
+      case('-H'):
+      case('-?'):
+        settings.help();
+        break;
+      default:
+        settings.outputHelp();
+        cb(opt);
+    }
+  };
+})();
+
+settings.parse(process.argv);
+
+console.log(settings.args);
